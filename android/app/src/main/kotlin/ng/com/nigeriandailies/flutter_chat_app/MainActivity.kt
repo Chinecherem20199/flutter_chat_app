8fa9724b1797edb73a332bea12a26a0a695216ab
neche <chi2064real@gmail.com>
2022-07-22 20:13:51 +0100

    worked on Animation

---
+++ android/app/src/main/kotlin/ng/com/nigeriandailies/flutter_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package ng.com.nigeriandailies.flutter_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screens/chat_screen.dart';
+import 'package:flutter_chat_app/screens/login_screen.dart';
+import 'package:flutter_chat_app/screens/registration_screen.dart';
+import 'package:flutter_chat_app/screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // theme: ThemeData.dark().copyWith(
+      //   textTheme: const TextTheme(
+      //       // bodyText1: TextStyle(color: Colors.black),
+      //       ),
+      // ),
+      // home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
